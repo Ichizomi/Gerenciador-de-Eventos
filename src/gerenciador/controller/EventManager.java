2aaaf50ec465ccd98bac964e731cd71f4897a77f
Ichizomi <rksgrijo@gmail.com>
2021-02-26 17:18:08 -0400

    Implementado Menu de Edição e Remoção de Pessoas do Banco. E respectivas operações de CRUD.

--- src/gerenciador/controller/EventManager.java
+++ src/gerenciador/controller/EventManager.java
@@ -2,33 +2,301 @@ package gerenciador.controller;
 
 import gerenciador.model.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 // Classe principal controlador do sistema. Inclui funcionalidade para gerênciar um evento de treinamento de uma empresa fictícia.
 public class EventManager {
 
 	private static ConnectDB conn = new ConnectDB(); // Conexão com o Banco.
+	private static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
 		//Estabelece conexão com o banco.
-		conn.connect();
-		
-		
-		// Teste de Execução
-		System.out.println("Hello World!");
-		
-		Person p = new Person("Lucas", "Grijó");
-		System.out.println("Criado por: " + p.getFirstName() + " " + p.getSurName());
-		
-		
-		// Salvar no banco.
-		p.SavePerson(conn, p);
-		
-		// Limpar todas as tabelas do banco.
-		//conn.ClearDatabase(conn);
+		conn.connect();	
+
+		ShowMenu0(); // Carrega Menu Inicial (Por Texto)
 		
 		// Encerra conexão com o banco.
 		conn.disconnect();
 		
+		// Limpar todas as tabelas do banco.
+		//conn.ClearDatabase(conn);
 	}
-
+	
+	// Menu de interação inicial do sistema. (Por Texto)
+	private static void ShowMenu0() {
+		System.out.println("--- Sistema de Gerênciamento de Eventos ---");		
+		System.out.println("1) Consultar/Listar Dados");
+		System.out.println("2) Cadastrar/Alterar/Remover Dados");
+		System.out.println("--- ----------------------------------- ---");
+		System.out.print("Digite o número da opção desejada: ");
+		int choice = ReadOption();
+		System.out.println();
+		switch(choice) {
+			case 1:
+				System.out.println("1 escolhido!");
+				Person.ListPeople(conn);
+				ShowMenu0();
+				break;
+			case 2:
+				ShowMenu2();
+				break;
+			default:
+				System.out.println("Opção inválida! Tente novamente");
+				ShowMenu0();
+				break;
+		}				
+	}
+	
+	// Menu para Opção 2 - Cadastro de Dados (Por Texto)
+	private static void ShowMenu2() {
+		System.out.println("--- Cadastrar/Alterar/Remover Dados ---");		
+		System.out.println("1) Cadastrar Sala");
+		System.out.println("2) Editar Sala");
+		System.out.println("3) Remover Sala");
+		System.out.println("4) Cadastrar Pessoa");
+		System.out.println("5) Editar Pessoa");
+		System.out.println("6) Remover Pessoa");
+		System.out.println("7) Voltar ao menu inicial...");
+		System.out.println("--- ----------------------------------- ---");
+		System.out.print("Digite o número da opção desejada: ");
+		int choice = ReadOption();
+		System.out.println();
+		scan.nextLine(); //Limpa Scanner
+		int subchoice;
+		int rId;
+		Person p = new Person();
+		String firstName;
+		String surName;
+		switch(choice) {
+			case 1:
+				System.out.println("1 escolhido!");
+				break;
+			case 2:
+				System.out.println("2 escolhido!");
+				break;
+			case 3:
+				System.out.println("3 escolhido!");
+				break;
+			case 4:
+				System.out.println("--- Cadastrar Pessoa ---");
+				System.out.print("Entre com o primeiro nome: ");
+				firstName = scan.nextLine();
+				System.out.print("Entre com o sobrenome: ");
+				surName = scan.nextLine();
+				p = new Person(firstName, surName);
+				p.SavePerson(conn, p); // Salvar no banco.	
+				ShowMenu2();
+				break;
+			case 5:
+				System.out.println("--- Editar Pessoa ---");
+				System.out.println("Escolher o método utilizado para a editar os dados de uma pessoa: ");
+				System.out.println("1) Informar o ID diretamente");
+				System.out.println("2) Listar pessoas e escolher entre elas");
+				System.out.println("3) Buscar pessoa");
+				subchoice = ReadOption();
+				rId = -1;				
+				switch(subchoice) {
+					case 1:
+						System.out.print("Entre com o ID: ");
+						rId = ReadOption();
+						scan.nextLine(); // Limpa Scanner.
+						if(rId != -1) {
+							System.out.print("Entre com novo nome: ");
+							firstName = scan.nextLine();
+							//scan.nextLine(); // Limpa Scanner.
+							System.out.print("Entre com novo sobrenome: ");
+							surName = scan.nextLine();
+							p = new Person(rId, firstName, surName);
+							System.out.println("Alterando para: " + firstName + " " + surName);
+							Person.UpdatePerson(conn, rId, p);
+						} else {
+							System.out.println("ID Inválido");	
+							ShowMenu0();
+						}
+						ShowMenu2();
+						break;
+					case 2:
+						rId = ListFromDBAndPickOption("--- Digite o ID da pessoa que deseja remover: ");
+						if(rId != -1) {
+							System.out.print("Entre com novo nome: ");
+							firstName = scan.nextLine();
+							System.out.print("Entre com novo sobrenome: ");
+							surName = scan.nextLine();
+							p = new Person(rId, firstName, surName);
+							System.out.println("Alterando para: " + firstName + " " + surName);
+							Person.UpdatePerson(conn, rId, p);
+						}
+						ShowMenu2();						
+						break;
+					case 3:
+						p = SearchPersonMenu();
+						if(p.getId() != -1) {
+							rId = p.getId();
+							System.out.println("Encontrado: [" + p.getId() + "] "+ p.getFirstName() +" - " + p.getSurName() + "");
+							System.out.print("Entre com novo nome: ");
+							firstName = scan.nextLine();
+							System.out.print("Entre com novo sobrenome: ");
+							surName = scan.nextLine();
+							p = new Person(rId, firstName, surName);
+							System.out.println("Alterando para: " + firstName + " " + surName);
+							Person.UpdatePerson(conn, rId, p);
+						} else {
+							System.out.println("Falha na busca. ID Inválido");
+						}			
+						ShowMenu2();
+						break;
+					default:
+						System.out.println("Opção inválida! Tente novamente");
+						System.out.println("ID Inválido");
+						ShowMenu0();
+						break;
+				}
+				break;
+			case 6:
+				System.out.println("--- Remover Pessoa ---");
+				System.out.println("Escolher o método utilizado para a remoção da pessoa: ");
+				System.out.println("1) Informar o ID diretamente");
+				System.out.println("2) Listar pessoas e escolher entre elas");
+				System.out.println("3) Buscar pessoa");
+				subchoice = ReadOption();
+				rId = -1;
+				switch(subchoice) {
+					case 1:
+						System.out.print("Entre com o ID: ");
+						rId = ReadOption();
+						if(rId != -1) {
+							Person.RemovePersonByID(conn, rId);
+						} else {
+							System.out.println("ID Inválido");							
+						}
+						ShowMenu2();
+						break;
+					case 2:
+						rId = ListFromDBAndPickOption("--- Digite o ID da pessoa que deseja remover: ");
+						if(rId != -1) {
+							Person.RemovePersonByID(conn, rId);
+						}
+						ShowMenu2();						
+						break;
+					case 3:
+						p = SearchPersonMenu();
+						if(p.getId() != -1) {
+							rId = p.getId();
+							System.out.println("Encontrado: [" + p.getId() + "] "+ p.getFirstName() +" - " + p.getSurName() + "");
+							Person.RemovePersonByID(conn, rId);
+						} else {
+							System.out.println("Falha na busca. ID Inválido");
+						}			
+						ShowMenu2();
+						break;
+					default:
+						System.out.println("Opção inválida! Tente novamente");
+						System.out.println("ID Inválido");
+						ShowMenu0();
+						break;
+				}
+				break;
+			case 7:
+				ShowMenu0();
+				break;
+			default:
+				System.out.println("Opção inválida! Tente novamente");
+				ShowMenu0();
+				break;
+		}
+	}
+	
+	// Leitura de uma Opção de Menu (Inteiro)
+	private static int ReadOption() {
+		if(scan.hasNextInt()) {
+			return scan.nextInt();
+		} else {
+			scan.nextLine(); // Limpa scanner.
+			return -1;
+		}
+	}
+	
+	// Leitura de um Campo de Texto
+	private static String ReadText(int maxSize) {
+		if(scan.hasNext()) {
+			return scan.nextLine();
+		} else {
+			scan.nextLine(); // Limpa scanner.
+			return "Invalido";
+		}
+	}
+	
+	// Lista dados do banco de dados e permite escolher um ID entre as opções listadas.
+	private static int ListFromDBAndPickOption(String operationMessage){
+		
+		// Listagem de Opções
+		List<Person> listp = Person.ListPeople(conn);
+		List<Integer> options = new ArrayList<>();
+		System.out.println("--- Listagem de Pessoas ---");		
+		System.out.println("[ID] Nome - Sobrenome");
+		for(int i=0; i < listp.size(); i++) {
+			System.out.println("[" + listp.get(i).getId() + "] "+ listp.get(i).getFirstName() +" - " + listp.get(i).getSurName() + "");
+			options.add(listp.get(i).getId());
+		}				
+		
+		// Validação da Opção
+		int choice = -1;
+		if(!options.isEmpty()) {
+			System.out.print(operationMessage);
+			choice = ReadOption();
+			scan.nextLine(); // Limpa Scanner.
+		} else {
+			System.out.println("--- Lista Vazia... ---");
+		}		
+		return choice;
+	}	
+	
+	// Abre menu para busca de uma pessoa (por nome ou sobrenome).
+	private static Person SearchPersonMenu() {
+		Person p = new Person();
+		System.out.println("Escolha o método da busca: ");
+		System.out.println("1) Buscar por primeiro nome");
+		System.out.println("2) Buscar por sobrenome");
+		System.out.println("3) Voltar ao menu inicial...");
+		int subchoice = ReadOption();
+		switch(subchoice) {
+			case 1:
+				System.out.print("Entre com o primeiro nome: ");
+				scan.nextLine(); // Limpa o scanner.
+				String firstName = scan.nextLine();
+				p = Person.SearchPersonByFirstName(conn, firstName);
+				if(p.hasValidId()) {
+					return p;
+				} else {
+					System.out.println("Pessoa não encontrada...");
+					ShowMenu0();
+				}
+				break;
+			case 2:
+				System.out.print("Entre com o sobrenome: ");
+				scan.nextLine(); // Limpa o scanner.
+				String surName = scan.nextLine();
+				p = Person.SearchPersonByFirstName(conn, surName);
+				if(p.hasValidId()) {
+					return p;
+				} else {
+					System.out.println("Pessoa não encontrada...");
+					ShowMenu0();
+				}
+				break;
+			case 3:
+				ShowMenu0();
+				break;
+			default:
+				System.out.println("Opção inválida! Tente novamente");
+				ShowMenu0();
+				break;
+		}
+		return p;
+	}
+	
 }
--- src/gerenciador/model/Person.java
+++ src/gerenciador/model/Person.java
@@ -1,23 +1,48 @@
 package gerenciador.model;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 // Uma pessoa que participa do evento.
 public class Person {
 
 	// Vars
+	private int id; // ID
 	private String firstName; // Primeiro Nome
 	private String surName; // Sobrenome
 	
 	// Método Construtor
 	public Person(String firstName, String surName) {
 		super();
+		this.id = -1;
 		this.firstName = firstName;
 		this.surName = surName;
 	}
 	
+	public Person(int id, String firstName, String surName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.surName = surName;
+	}
+	
+	public Person() {
+		super();
+		this.id = -1;
+		this.firstName = "N/A";
+		this.surName = "N/A";
+	}
+	
 	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -30,6 +55,13 @@ public class Person {
 	public void setSurName(String surName) {
 		this.surName = surName;
 	}
+	public boolean hasValidId() {
+		if(this.id != -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	// Adiciona uma pessoa ao banco de dados.
 	public void SavePerson(ConnectDB connection, Person p) {
@@ -38,7 +70,7 @@ public class Person {
 		    PreparedStatement statement = connection.connect().prepareStatement(sql);
 			int status = statement.executeUpdate();
 			if(status != 0) {
-				System.out.println("Pessoa adicionada com sucesso.");
+				System.out.println("Pessoa \"" + p.getFirstName() + " " + p.getSurName() + "\" adicionada com sucesso.");
 			}
 		} catch (SQLException e) {
 		    e.printStackTrace();
@@ -47,15 +79,105 @@ public class Person {
 		}
 	}
 	
-	// Consultar pessoas no banco de dados.
-	public void SearchPersonByFirstName(String firstName) {
-		//To-Do
-	}
-	public void SearchPersonByLastName(String lastName) {
-		//To-Do
-	}
-	public void SearchPersonByFullName(String fullName) {
-		//To-Do
+	// Retorna uma lista de todas as pessoas cadastradas no banco.
+	public static List<Person> ListPeople(ConnectDB connection) {
+		List<Person> list = new ArrayList<>();
+		String sql = "SELECT * from person;";	
+		
+		try {
+		    PreparedStatement statement = connection.connect().prepareStatement(sql);
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()) {
+				int idPerson = rs.getInt("idPerson");
+				String firstName = rs.getString("firstName");
+				String surName = rs.getString("surName");
+				System.out.println(idPerson + " " + firstName + " " + surName);
+				Person p = new Person(idPerson, firstName, surName);
+				list.add(p);
+			}
+			return list;
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		    return list;
+		} finally {
+			connection.disconnect();
+		}
 	}
 	
+	// Consultar pessoas no banco de dados (por primeiro nome).
+	public static Person SearchPersonByFirstName(ConnectDB connection, String _firstName) {
+		Person p = new Person();
+		String sql = "SELECT * from person where firstName = '" + _firstName + "';";			
+		try {
+		    PreparedStatement statement = connection.connect().prepareStatement(sql);
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()) {
+				int idPerson = rs.getInt("idPerson");
+				String firstName = rs.getString("firstName");
+				String surName = rs.getString("surName");
+				//System.out.println(idPerson + " " + firstName + " " + surName);
+				p = new Person(idPerson, firstName, surName);
+			}
+			return p;
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		    return p;
+		} finally {
+			connection.disconnect();
+		}
+	}
+	// Consultar pessoas no banco de dados (por sobrenome).
+	public static Person SearchPersonByLastName(ConnectDB connection, String _surName) {
+		Person p = new Person();
+		String sql = "SELECT * from person where surName = '" + _surName + "';";			
+		try {
+		    PreparedStatement statement = connection.connect().prepareStatement(sql);
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()) {
+				int idPerson = rs.getInt("idPerson");
+				String firstName = rs.getString("firstName");
+				String surName = rs.getString("surName");
+				//System.out.println(idPerson + " " + firstName + " " + surName);
+				p = new Person(idPerson, firstName, surName);
+			}
+			return p;
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		    return p;
+		} finally {
+			connection.disconnect();
+		}
+	}
+	
+	// Remove pessoa do banco de dados (por ID).
+	public static void RemovePersonByID(ConnectDB connection, int id) {
+		String sql = "DELETE from person where idPerson=" + id + ";";		
+		try {
+		    PreparedStatement statement = connection.connect().prepareStatement(sql);
+			int status = statement.executeUpdate();
+			if(status != 0) {
+				System.out.println("Pessoa deletada.");
+			}
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+	}
+	
+	// Atualiza dados de uma pessoa no banco de dados (por ID)
+	public static void UpdatePerson(ConnectDB connection, int id, Person updatedPerson) {
+		String sql = "UPDATE person SET firstName = '" + updatedPerson.getFirstName() + "', surName = '" + updatedPerson.getSurName() + "' where idPerson = " + id + ";";			
+		try {
+		    PreparedStatement statement = connection.connect().prepareStatement(sql);
+			int status = statement.executeUpdate();
+			if(status != 0) {
+				System.out.println("Dados atualizados.");
+			}
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+	}
 }
